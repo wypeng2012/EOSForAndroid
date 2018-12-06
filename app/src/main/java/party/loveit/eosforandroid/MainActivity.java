a682971672cec5c3cc92a9d57f69af4fcf2ec6f1
wangyunpeng@snail.com <wangyunpeng@snail.com>
2018-12-06 15:06:48 +0800

    update

--- app/src/main/java/party/loveit/eosforandroid/MainActivity.java
+++ app/src/main/java/party/loveit/eosforandroid/MainActivity.java
@@ -2,6 +2,13 @@ package party.loveit.eosforandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import party.loveit.bip44forandroidlibrary.utils.Bip44Utils;
+import party.loveit.eosforandroidlibrary.Ecc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    List<String> words = Bip44Utils.generateMnemonicWords(MainActivity.this);
+                    Log.e("TAG", "words: " + words.toString());
+
+
+                    BigInteger pri = Bip44Utils.getPathPrivateKey(words,"m/44'/194'/0'/0/0");
+                    Log.e("TAG", "pri1: " + pri.toString(16));
+
+                    String eospk =  Ecc.seedPrivate(pri);
+                    Log.e("TAG", "EOS privateKey: " + eospk);
+
+                    String eospuk =  Ecc.privateToPublic(eospk);
+                    Log.e("TAG", "EOS publicKey: " + eospuk);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("TAG", "Exception: " + e);
+                }
+            }
+        }).start();
     }
 }
